6aa08e245828061f47c50fc1d4112f45a216573f
arthur-013 <aglanville19@gmail.com>
2021-08-21 10:28:37 +0100

    second commit

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,8 +4,8 @@ class ListContacts extends Component{
     render () {
         return (
             <ol className ='contact-list'>
-                {this.props.contacts.map((contact)=>
-                (<li key = {contact.id} className = 'contact-list-item'>
+                {this.props.contacts.map((contact)=>(
+                <li key = {contact.id} className = 'contact-list-item'>
                     <div 
                     className = 'contact-avatar'
                     style = {{
